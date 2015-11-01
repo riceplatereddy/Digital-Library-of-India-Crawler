0bef61bde568900f686ec7028ff6c5085e43b145
EC2 Default User <ec2-user@ip-172-31-30-194.us-west-2.compute.internal>
2015-11-01 05:32:42 +0000

    Version 2

--- crawler.java
+++ crawler.java
@@ -8,12 +8,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.LineIterator;
-import org.apache.commons.io.filefilter.SuffixFileFilter;
 
 public class Crawler2 {
 
